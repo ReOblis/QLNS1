f8de87f604fb2f5b6d46370e3ca678ffd852a6b5
ReOblis <oblis.re@gmail.com>
2023-05-07 17:32:38 +0700

    Add project files.

---
+++ QLNS/Data/QLNSContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QLNS.Models;
+
+namespace QLNS.Data
+{
+    public class QLNSContext : DbContext
+    {
+        public QLNSContext (DbContextOptions<QLNSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<QLNS.Models.ChamCong> ChamCong { get; set; }
+        public DbSet<QLNS.Models.NhanVien> NhanVien { get; set; } = default!;
+    }
+}
